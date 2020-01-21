35ca7c17bb3f639812f59918ba4e5c41161aa547
Diogo Agra Alexandria <diogoagrasmsdc@gmail.com>
2020-01-20 21:55:57 -0300

    Liberação de cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(cors());
+// app.use(cors({ origin: 'http://localhost:3000' }));
 
 mongoose.connect('mongodb+srv://admin:Diogo0502@cluster0-w2pqv.mongodb.net/week-10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -13,5 +16,5 @@ mongoose.connect('mongodb+srv://admin:Diogo0502@cluster0-w2pqv.mongodb.net/week-
 });
 
 app.listen(3001, () => {
-    console.log('Server listening on port 3000.');
+    console.log('Server listening on port 3001.');
 });
